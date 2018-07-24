4ac2d669d4c1fd6e52d2098fdd3fe1f55fda20a5
Kazuhito Hokamura <kazuhito-hokamura@cookpad.com>
2018-07-24 16:27:14 +0900

    Improve message

--- scripts/init.js
+++ scripts/init.js
@@ -2,15 +2,24 @@
 
 const fs = require('fs-extra');
 const path = require('path');
+const { execSync } = require('child_process');
 
-module.exports = function(appPath, appName, verbose, originalDirectory) {
+module.exports = function(appPath, appName) {
   const ownPackageName = require(path.join(__dirname, '..', 'package.json')).name;
   const ownPath = path.join(appPath, 'node_modules', ownPackageName);
   const appPackage = require(path.join(appPath, 'package.json'));
-  const useYarn = fs.existsSync(path.join(appPath, 'yarn.lock'));
 
   appPackage.scripts = { start: 'webpack-dev-server' };
   fs.writeFileSync(path.join(appPath, 'package.json'), JSON.stringify(appPackage, null, 2));
-
   fs.copySync(path.join(ownPath, 'template'), appPath);
+
+  execSync('yarn', { stdio: 'inherit' });
+
+  console.log();
+  console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log();
+  console.log('We suggest that you begin by typing:');
+  console.log();
+  console.log(`  $ cd ${appName}`);
+  console.log('  $ yarn start');
 };
